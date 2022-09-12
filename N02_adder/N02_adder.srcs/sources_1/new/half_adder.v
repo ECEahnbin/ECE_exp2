b342d5248a5dd9108be99f7240a1ff5a35fd93b3
ECEahnbin <ahnbin2008@uos.ac.kr>
2022-09-12 21:13:19 +0900

    half_adder_logic

--- N02_adder/N02_adder.srcs/sources_1/new/half_adder.v
+++ N02_adder/N02_adder.srcs/sources_1/new/half_adder.v
@@ -1,13 +1,8 @@
 module half_adder(x,y,c,s);
 input x, y;
-output reg c, s; // always문을 사용할 때는 reg를 써줘야함.
+output c, s; // always문을 사용할 때는 reg를 써줘야함.
 
-always @(*) begin
-    case({x,y})
-        2'b00 : {c,s} <= 2'b00;
-        2'b01 : {c,s} <= 2'b01;
-        2'b10 : {c,s} <= 2'b01;
-        default : {c,s} <= 2'b10;
-    endcase
-end
+assign c = x & y;
+assign s = x ^ y;
+
 endmodule
