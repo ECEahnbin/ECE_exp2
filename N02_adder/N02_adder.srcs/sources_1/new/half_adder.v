e17918b67ebc1a137bf6aada28bb213b374a3b62
ECEahnbin <ahnbin2008@uos.ac.kr>
2022-09-12 18:26:52 +0900

    Case code

--- N02_adder/N02_adder.srcs/sources_1/new/half_adder.v
+++ N02_adder/N02_adder.srcs/sources_1/new/half_adder.v
@@ -1,8 +1,13 @@
 module half_adder(x,y,c,s);
 input x, y;
-output c, s;
+output reg c, s; // always문을 사용할 때는 reg를 써줘야함.
 
-assign c = x & y;
-assign s = x ^ y;
-
+always @(*) begin
+    case({x,y})
+        2'b00 : {c,s} <= 2'b00;
+        2'b01 : {c,s} <= 2'b01;
+        2'b10 : {c,s} <= 2'b01;
+        default : {c,s} <= 2'b10;
+    endcase
+end
 endmodule
